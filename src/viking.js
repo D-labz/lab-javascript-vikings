c9411a12b194dc02c1379211d862282b3dc66d71
Dmytro Labenskyy <d.mon911@gmail.com>
2024-07-16 23:15:24 +0100

    Solved Lab

--- src/viking.js
+++ src/viking.js
@@ -74,8 +74,48 @@ class War {
   should remove dead Saxons from the army
   should return result of calling receiveDamage() of a Saxon with the strength of a Viking */
   vikingAttack() {
-    let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    const report = this.saxonArmy[randomSaxon].receiveDamage(
+      this.vikingArmy[randomViking].strength
+    );
+    if (report === `A Saxon has died in combat`) {
+      this.saxonArmy.splice(randomSaxon, 1);
+      return report;
+    } else {
+      return report;
+    }
   }
-  /* saxonAttack()
-showStatus()   */
+
+  saxonAttack() {
+    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    const report = this.vikingArmy[randomViking].receiveDamage(
+      this.saxonArmy[randomSaxon].strength
+    );
+    if (
+      report ===
+      `${this.vikingArmy[randomViking].name} has died in act of combat`
+    ) {
+      this.vikingArmy.splice(randomViking, 1);
+      return report;
+    } else {
+      return report;
+    }
+  }
+
+  ///should be a function
+  /* hould receive 0 arguments
+if the Saxon array is empty, should return "Vikings have won the war of the century!"
+if the Viking array is empty, should return "Saxons have fought for their lives and survived another day..."
+if there are at least 1 Viking and 1 Saxon, should return "Vikings and Saxons are still in the thick of battle." */
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return `Vikings have won the war of the century!`;
+    } else if (this.vikingArmy.length === 0) {
+      return `Saxons have fought for their lives and survived another day...`;
+    } else if (this.saxonArmy.length > 0 && this.saxonArmy.length > 0) {
+      return `Vikings and Saxons are still in the thick of battle.`;
+    }
+  }
 }
